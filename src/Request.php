49c74937db9d25ba8ea5151192a35fef732708ec
Ján Forgáč <forgac@artfocus.cz>
2020-03-02 01:54:22 +0100

    Request: catch and handle Timeout and other Request Exceptions

--- src/Request.php
+++ src/Request.php
@@ -15,7 +15,7 @@ class Request
 {
 
 	const API_HOST = 'https://zp.toptrans.cz';
-	const API_BASE_PATH = '/api';
+	const API_BASE_PATH = '/api/json';
 
 	const CONNECTION_TIMEOUT = 2; // sec
 	const TIMEOUT = 4; // sec
@@ -40,23 +40,27 @@ class Request
 
 	public function sentRequest(array $arrayData, string $methodPath): array
 	{
-		$client = new GuzzleHttp\Client(['base_uri' => self::API_HOST]);
-		$response = $client->request('POST', self::API_BASE_PATH . $methodPath, [
-			'auth' => [$this->username, $this->password],
-			'connect_timeout' => $this->connectionTimeout,
-			'timeout' => $this->timeout,
-			'http_errors' => false,
-			'body' => json_encode($arrayData),
-		]);
+		try {
+			$client = new GuzzleHttp\Client(['base_uri' => self::API_HOST]);
+			$response = $client->request('POST', self::API_BASE_PATH . $methodPath, [
+				'auth' => [$this->username, $this->password],
+				'connect_timeout' => $this->connectionTimeout,
+				'timeout' => $this->timeout,
+				'http_errors' => false,
+				'body' => json_encode($arrayData),
+			]);
 
-		if ($response->getStatusCode() !== 200) {
-			throw new ResponseStatusException('Wrong response code: ' . $response->getStatusCode());
-		}
+			if ($response->getStatusCode() !== 200) {
+				throw new ResponseStatusException('Wrong response code: ' . $response->getStatusCode());
+			}
 
-		$responseBody = $response->getBody()->getContents();
-		$decodedResponse = json_decode($responseBody);
-		if (!$decodedResponse) {
-			throw new BadResponseException($responseBody);
+			$responseBody = $response->getBody()->getContents();
+			$decodedResponse = json_decode($responseBody, true);
+			if (!$decodedResponse) {
+				throw new BadResponseException($responseBody);
+			}
+		} catch (GuzzleHttp\Exception\RequestException $e) {
+			$decodedResponse['errors'][] = $e->getMessage();
 		}
 
 		return $decodedResponse;
